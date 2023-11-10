e4a06b25782c3bf22674c349fb961298436d7608
alejo688 <alejandroramirez688@gmail.com>
2023-11-09 19:01:20 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Javier Alejandro Ramirez Garcia on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
